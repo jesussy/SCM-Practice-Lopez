22c258d5c6fab5f510d9355f90b49833c0b34d1a
Jesus Lopez <103091239+jesussy@users.noreply.github.com>
2025-03-31 21:46:46 -0500

    Update happymessage.py

--- happymessage.py
+++ happymessage.py
@@ -1,5 +1,9 @@
 def happymessage(name):
     print("You're doing great ", name, "! Do what makes you happy.", sep="")
 
+def midmessage(name):
+    print("You are doing satisfactory ", name, ". Do what is required of you.", sep="")
+
 happymessage("Jesus")
 happymessage("Dr. Wolfe")
+midmessage("John Doe")
